faa2902d3b11804ddef46ac3ff6e6c26660a793c
Cotterell Anna <cotterellanna@gmail.com>
2023-04-28 02:44:58 +0100

    server

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const encrypt = require("mongoose-encryption");
 const session = require("express-session");
 const { log } = require("console");
 const http = require("http");
-const { hostname } = require("os");
 const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 
@@ -1410,6 +1409,6 @@ app.get("/admin/logout", function(req,res){
     req.session.loggedIn = false;
     res.redirect("/admin/login");
 })
-server.listen(port, hostname:()=>{
+server.listen(port, function(){
     console.log(`Server started on port ${port}`)
 })
